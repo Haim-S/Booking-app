a0522d30b7f25527ce0a57e7de2bf3371955ac93
HaimSlbah <26life72@gmail.com>
2023-06-11 20:38:33 +0300

    fix the contro hotel

---
+++ backend/src/controllers/hotel.controller.js
@@ -0,0 +1,12 @@
+import Hotel from "../models/Hotel.js";
+
+export const createHotel = async (req, res, next)=> {
+    const newHotel = Hotel.create(req.body);
+    res.status(200).json(newHotel);
+}
+
+export const updateHotel = async (req, res, next) => {
+    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
+    res.status(200).json(updatedHotel);
+}
+
