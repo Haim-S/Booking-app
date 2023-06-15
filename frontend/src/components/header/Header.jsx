bc51eb082bace9f64b5decc91bc555366af80ef8
HaimSlbah <26life72@gmail.com>
2023-06-16 01:10:23 +0300

    started the header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./header.css"
 import {RiHotelBedFill} from "react-icons/ri";
+import {IoIosAirplane} from "react-icons/io";
+import {AiFillCar} from "react-icons/ai";
+import {FaTaxi} from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -8,6 +11,23 @@ const Header = () => {
         <div className="headerList">
             <div className="headerListItem">
             <RiHotelBedFill size={30}/>
+            <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+            <IoIosAirplane size={30}/>
+            <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+            <AiFillCar size={30}/>
+            <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+            <RiHotelBedFill size={30}/>
+            <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+            <FaTaxi size={30}/>
+            <span>Airport taxis</span>
             </div>
         </div>
     </header>
