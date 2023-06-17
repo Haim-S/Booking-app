02f16509413a691a105c812b0085e403c3df5a0e
HaimSlbah <26life72@gmail.com>
2023-06-17 22:32:30 +0300

    header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -6,30 +6,38 @@ import {AiFillCar} from "react-icons/ai";
 import {FaTaxi} from "react-icons/fa";
 
 const Header = () => {
+
+  const iconSize = 25;
+
   return (
     <header>
+      <div className="headerContainer">
         <div className="headerList">
-            <div className="headerListItem">
-            <RiHotelBedFill size={30}/>
+            <div className="headerListItem active">
+            <RiHotelBedFill size={iconSize}/>
             <span>Stays</span>
             </div>
             <div className="headerListItem">
-            <IoIosAirplane size={30}/>
+            <IoIosAirplane size={iconSize}/>
             <span>Flights</span>
             </div>
             <div className="headerListItem">
-            <AiFillCar size={30}/>
+            <AiFillCar size={iconSize}/>
             <span>Car rentals</span>
             </div>
             <div className="headerListItem">
-            <RiHotelBedFill size={30}/>
+            <RiHotelBedFill size={iconSize}/>
             <span>Attractions</span>
             </div>
             <div className="headerListItem">
-            <FaTaxi size={30}/>
+            <FaTaxi size={iconSize}/>
             <span>Airport taxis</span>
             </div>
         </div>
+        <h1 className='headerTitle'>A lifetime of discounts? It's Genius.</h1>
+        <p className='headerDesc'> more with a free lamabooking account</p>
+        <button className='headerbtn'>sign in/ Register</button>
+      </div>
     </header>
   )
 }
