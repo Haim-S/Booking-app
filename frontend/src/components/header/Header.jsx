d9c5306613ed752c55ab2825c22e2d116cfbe78d
HaimSlbah <26life72@gmail.com>
2023-06-18 00:39:19 +0300

    header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -6,37 +6,46 @@ import {AiFillCar} from "react-icons/ai";
 import {FaTaxi} from "react-icons/fa";
 
 const Header = () => {
-
   const iconSize = 25;
+
+  const headerPlaceholder = [
+    {value: "Stays", icon: "HotelBed"},
+    {value: "Flights", icon:  "Airplane"},
+    {value: "Car rentals", icon: "FillCar" },
+    {value: "Attractions", icon: "HotelBed"},
+    {value: "Airport taxis", icon: "Taxi"},
+  
+  ]
+
 
   return (
     <header>
       <div className="headerContainer">
         <div className="headerList">
+          {headerPlaceholder.map((val, i)=>{
+            return(
             <div className="headerListItem active">
-            <RiHotelBedFill size={iconSize}/>
-            <span>Stays</span>
+            {val.icon === "HotelBed" &&  <RiHotelBedFill size={iconSize}/>}
+            {val.icon === "Airplane" &&  <IoIosAirplane      size={iconSize}/>}
+            {val.icon === "FillCar" &&  <AiFillCar      size={iconSize}/>}
+            {val.icon === "HotelBed" &&   <RiHotelBedFill size={iconSize}/>}
+            {val.icon === "Taxi" &&  <FaTaxi        size={iconSize}/>}
+            <span>{val.value}</span>
             </div>
-            <div className="headerListItem">
-            <IoIosAirplane size={iconSize}/>
-            <span>Flights</span>
-            </div>
-            <div className="headerListItem">
-            <AiFillCar size={iconSize}/>
-            <span>Car rentals</span>
-            </div>
-            <div className="headerListItem">
-            <RiHotelBedFill size={iconSize}/>
-            <span>Attractions</span>
-            </div>
-            <div className="headerListItem">
-            <FaTaxi size={iconSize}/>
-            <span>Airport taxis</span>
-            </div>
+            )
+          })}
         </div>
         <h1 className='headerTitle'>A lifetime of discounts? It's Genius.</h1>
         <p className='headerDesc'> more with a free lamabooking account</p>
         <button className='headerbtn'>sign in/ Register</button>
+        <div className="headerSearch">
+        <RiHotelBedFill size={iconSize}/>
+        <input 
+        type="text" 
+        placeholder="Where are you going?"
+        className='headerSearchInput'
+        />
+        </div>
       </div>
     </header>
   )
